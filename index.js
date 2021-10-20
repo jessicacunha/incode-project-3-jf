d4de2b96188ca5d4214dcac1d86a635d6bf0e075
BigTimeCoffee <padraigdowling1@hotmail.com>
2021-10-20 11:33:47 -0300

    test.push main

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+const { schedules } = require('../../../AppData/Local/Temp/Temp1_data.zip/data');
 var data = require('./data');
 
 app.get('/', (req, res) => {
@@ -21,6 +22,17 @@ app.get('/users', (req, res) => {
 app.get('/schedules', (req, res) => {
   res.send(data.schedules);
 });
+// used req.params for route
+app.get('/users/:id', (req, res) => {
+    let id = req.params.id; 
+    console.log(id);
+    if(data.users[id]) {res.json(data.users[id])}
+    else {
+        res.json('Unknown')
+    }
+  })
 
 
 app.listen(3000, function () { });
+
+//test push to main

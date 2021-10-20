136adb7564a7e70389029876607f79f6f9dacb07
palhaogv <palhaogv@gmail.com>
2021-10-20 17:43:19 +0200

    updating the schedules routes

--- index.js
+++ index.js
@@ -1,39 +1,35 @@
 var express = require('express');
 var app = express();
-const { schedules } = require('../../../AppData/Local/Temp/Temp1_data.zip/data');
 var data = require('./data');
 
 app.get('/', (req, res) => {
-  res.send('Welcome to our schedule website');
+    res.send('Welcome to our schedule website');
 });
 
 app.get('/users', (req, res) => {
-  res.send(data.users);
+    res.send(data.users);
 });
-
-// app.get('/users/id', (req, res) => {
-//   var id = data.users.user_id
-
-
-
-//   res.send(data.users);
-// });
 
 app.get('/schedules', (req, res) => {
-  res.send(data.schedules);
+    res.send(data.schedules);
 });
-// used req.params for route
+
 app.get('/users/:id', (req, res) => {
     let id = req.params.id; 
-    console.log(id);
     if(data.users[id]) {res.json(data.users[id])}
     else {
         res.json('Unknown')
     }
-  })
+})
 
+app.get(`/users/:userId/schedules`, (req, res) => {
+    let newSchedules = data.schedules.filter(obj => {
+        return req.params.userId == obj.user_id
+    })
+    console.log(newSchedules)
+    res.send(newSchedules)
+})
 
 app.listen(3000, function () { });
 
-//test push to main
-testing = []
+

a1c4fe4b555d87e2f1381cceec9d627568767a59
palhaogv <palhaogv@gmail.com>
2021-10-20 12:24:02 +0200

    fixing the for loop to create all the routes.

--- index.js
+++ index.js
@@ -1,26 +1,45 @@
 const express = require('express');
-
+const app = express();
 const data = require('./data');
 
-const app = express();
-const port = 3000;
 
 app.get('/', (req, res) => {
     res.send('Welcome to our schedule website')
 })
 
 //a 'for' to create a route for each user
-for (var user in data.users) {
+for (let user in data.users) {
     app.get(`/users/${user}`, (req, res) => {
         res.send(data.users[user])
     })
 }
 
-app.get('/users/0/schedules', (req, res) => {
-    res.send(schedules)
+app.get('/schedules', (req, res) => {
+    res.send(data.schedules);
+});
+
+app.listen(3000, () => {
+    console.log(`Server running at http://localhost:${3000}/`);
 })
 
-app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`);
+//route for the schedule fo each user
+/*for (var schedule in data.schedules) {
+data.schedules.filter(obj => {
+    console.log(obj)
+    
+    app.get(`/users/${obj.user_id}/schedules`, (req, res) => {
+        res.send(obj)
+    })
 })
+}
+
+var result = data.schedules.filter(obj => {
+    if (obj.user_id === 0) {
+        console.log(obj)
+    }
+    return obj.user_id === 0
+})*/
+
+
+
 

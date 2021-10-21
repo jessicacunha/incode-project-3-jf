1af7eb4b62855953cbee631372ac569ff4d9dac2
palhaogv <palhaogv@gmail.com>
2021-10-21 13:31:01 +0200

    updating POST for schedules and users

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var app = express();
 var data = require('./data');
+var bodyParser = require('body-parser');
+app.use(bodyParser.text());
 
 app.get('/', (req, res) => {
     res.send('Welcome to our schedule website');
@@ -16,8 +18,9 @@ app.get('/schedules', (req, res) => {
 
 app.get('/users/:id', (req, res) => {
     let id = req.params.id; 
-    if(data.users[id]) {res.json(data.users[id])}
-    else {
+    if(data.users[id]) {
+        res.json(data.users[id])
+    } else {
         res.json('Unknown')
     }
 })
@@ -26,8 +29,29 @@ app.get(`/users/:userId/schedules`, (req, res) => {
     let newSchedules = data.schedules.filter(obj => {
         return req.params.userId == obj.user_id
     })
-    console.log(newSchedules)
     res.send(newSchedules)
+})
+
+app.post('/schedules', (req, res) => {
+    let scheduleObject = {
+        'user_id':req.body.user_id, 
+        'day':req.body.day, 
+        'start_at':req.body.start_at, 
+        'end_at':req.body.end_at
+    }
+    data.schedules.push(scheduleObject)
+    res.send(scheduleObject)
+})
+
+app.post('/users', (req, res) => {
+    let newUser = {
+        firstname: req.body.firstname, 
+        lastname: req.body.lastname, 
+        email: req.body.email, 
+        password: req.body.password
+    }
+    data.users.push(newUser)
+    res.send(newUser)
 })
 
 app.listen(3000, function () { });

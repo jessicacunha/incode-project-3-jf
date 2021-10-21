8e2c0e874ca32574ddd9cd6f13665434ed5383d2
BigTimeCoffee <padraigdowling1@hotmail.com>
2021-10-21 08:10:21 -0300

    user/sched

--- index.js
+++ index.js
@@ -3,24 +3,33 @@ var app = express();
 var data = require('./data');
 
 app.get('/', (req, res) => {
-  res.send('Welcome to our schedule website');
+    res.send('Welcome to our schedule website');
 });
 
 app.get('/users', (req, res) => {
-  res.send(data.users);
+    res.send(data.users);
 });
-
-// app.get('/users/id', (req, res) => {
-//   var id = data.users.user_id
-
-
-
-//   res.send(data.users);
-// });
 
 app.get('/schedules', (req, res) => {
-  res.send(data.schedules);
+    res.send(data.schedules);
 });
 
+app.get('/users/:id', (req, res) => {
+    let id = req.params.id; 
+    if(data.users[id]) {res.json(data.users[id])}
+    else {
+        res.json('Unknown')
+    }
+})
+
+app.get(`/users/:userId/schedules`, (req, res) => {
+    let newSchedules = data.schedules.filter(obj => {
+        return req.params.userId == obj.user_id
+    })
+    console.log(newSchedules)
+    res.send(newSchedules)
+})
 
 app.listen(3000, function () { });
+
+
